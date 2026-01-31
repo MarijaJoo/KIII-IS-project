ca712fd332dba9cc6126e39d607da24159678a16
MarijaJoo <139081109+MarijaJoo@users.noreply.github.com>
2026-01-31 15:49:28 +0100

    Adding Dockerfile, fixing the errors that came with connecting to docker such as changing the connection string in appsettings.json

--- Is.Repository/obj/Debug/net10.0/Is.Repository.AssemblyInfo.cs
+++ Is.Repository/obj/Debug/net10.0/Is.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Is.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa562724e2cf9a214e0c086a0ed24322a1d82eab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Is.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Is.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
